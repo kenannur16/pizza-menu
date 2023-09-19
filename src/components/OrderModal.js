e1945c9f31a0c4dc493037c53410f78a82ae618d
unknown <kenannur16@gmail.com>
2023-09-19 22:08:03 +0300

    styling on extra ingridients

--- src/components/OrderModal.js
+++ src/components/OrderModal.js
@@ -1,8 +1,18 @@
 import { useState } from "react";
 import ingData from "./ingData.json";
+import pizzaData from "./pizzaData.json";
 
 export default function OrderModal({ order, modalOpen }) {
   const [click, setClick] = useState(false);
+  const [isSelect, setIsSelect] = useState(true);
+
+  const addToExtra = function (ing) {
+    if (isSelect) {
+      if (!pizzaData.setIng.includes(ing)) pizzaData.setIng.push(ing);
+    } else {
+      pizzaData.setIng.filter((item) => item !== ing);
+    }
+  };
   return (
     <div className={modalOpen ? "modal-overlay" : "hidden"}>
       <div className="modal-content">
@@ -25,22 +35,35 @@ export default function OrderModal({ order, modalOpen }) {
         >
           Select Extra Ingredients
         </button>
-        <div className={click ? "extra-Ing" : "hidden"}>
+        <div className={click ? "ing-box" : "hidden"}>
           {ingData.map((ing) => (
-            <div>
-              <input type="checkbox" name={ing.id} value={ing.name} />
-              <label for={ing.name}>{ing.name}</label>
-              <span>{ing.price} $</span>
-            </div>
+            <button
+              onClick={(ing) => {
+                isSelect ? setIsSelect(false) : setIsSelect(true);
+                addToExtra(ing.name);
+              }}
+              className={isSelect ? "btnIngBefore" : "hidden"}
+            >
+              {ing.name} {ing.price} $
+            </button>
           ))}
         </div>
 
-        <select className="btn-select-box" placeholder="select pizza size...">
-          <option className="option">select size...</option>
-          <option className="option">small</option>
-          <option className="option">medium</option>
-          <option className="option">xLarge</option>
-        </select>
+        <h1>pizza size</h1>
+        <hr />
+
+        {/* <button className="btn-select-box">select pizza size</button> */}
+        <div className="size-box">
+          <label class="custom-checkbox-label">
+            <input type="radio" name="option" value="option1" /> Small
+          </label>
+          <label class="custom-checkbox-label">
+            <input type="radio" name="option" value="option1" /> Medium
+          </label>
+          <label class="custom-checkbox-label">
+            <input type="radio" name="option" value="option1" /> Large
+          </label>
+        </div>
 
         <span className="order-header">12.45 $</span>
         <div className="btn-container">
